640be5674dcef6e8b1fc6817188ed24baae1e899
John Azinheira <jva357@gmail.com>
2020-10-30 10:50:55 -0400

    Update Nozzle Offsets (HeroMe)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,7 +987,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -39, -0.5, -1.88 }
+#define NOZZLE_TO_PROBE_OFFSET { -38, -5.8, -2.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
